218e31d498ae590e9d772a825e24f26762da94dd
Jain7 <RJain7@ad.harman.com>
2019-02-25 17:17:56 +0530

    Added delete functionality

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,28 @@
+const {MongoClient, ObjectId} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDb server.');
+    }
+    const db = client.db('TodoApp');
+    
+    // db.collection("Todos").deleteOne({
+    //     "Description": "Go to sleep at 00:00."
+    // }).then((doc) => {
+    //     console.log(JSON.stringify(doc, undefined, 2));
+    // });
+
+    // db.collection("Todos").findOneAndDelete({
+    //     "Description": "Go to sleep at 00:00."
+    // }).then((doc) => {
+    //     console.log(JSON.stringify(doc, undefined, 2));
+    // });
+
+    db.collection("Todos").deleteMany({
+        "Description": "Go to sleep at 00:00."
+    }).then((doc) => {
+        console.log(JSON.stringify(doc, undefined, 2));
+    });
+
+    client.close();
+});
