659ed6970f03e15b98ef4fad4d20e7b6e79550ba
Jain7 <RJain7@ad.harman.com>
2019-02-25 16:14:20 +0530

    Added users collection

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -2,17 +2,32 @@ const MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     if(err) {
-        return console.log('Unable to connect with TodoApp database!!!');
+        return console.log('Unable to connect to MongoDB server!!!');
     }
     console.log('Connected to TodoApp database.');
+    
+    // const db = client.db('TodoApp');
+    // db.collection('Todos').insertOne({
+    //     'taskNo': 4,
+    //     'Description': 'Go to sleep at 00:00.'
+    // }, (err, result) => {
+    //     if(err) {
+    //         return console.log('Unable to insert data into Todos collection!!!');
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
     const db = client.db('TodoApp');
-    db.collection('Todos').insertOne({
-        'taskNo': 4,
-        'Description': 'Go to sleep at 00:00.'
+    db.collection('Users').insertOne({
+        'name': "Ace Ackerman",
+        "age": 22,
+        "location": "Antiva"
     }, (err, result) => {
         if(err) {
-            return console.log('Unable to insert data into Todos collection!!!');
+            return console.log('Unable to add document to colletion Users. ', err);
         }
-        console.log('Document added to Todos collection!!!');
+        console.log(result.ops, undefined, 2);
     });
+
+    client.close();
 });
